a91009368323c1a9bab3d085132e922aac7aa4b1
Carl Persson <carl@j35.se>
2023-05-18 06:28:39 +0200

    Many bug-fixes and general changes

--- Multihead_attention.py
+++ Multihead_attention.py
@@ -30,17 +30,17 @@ class DotProductAttention(Layer):
         self.W_value = self.add_weight(shape=(input_shape[-1], self.value_size), initializer=initializer,
                                        trainable=True)
 
-    def call(self, inputs):
+    def call(self, inputs, *args, **kwargs):
         # Did not implement a mask - Don't know if it is required
 
         # Compute the Q, K, V matrices as given in attention is all you need
         Query = tf.matmul(inputs, self.W_query)
         Key = tf.matmul(inputs, self.W_key)
         Value = tf.matmul(inputs, self.W_value)
-
         # Calculate similarity measure and apply softmax along columns
-        similarity = tf.nn.softmax(tf.matmul(Query, tf.transpose(Key)), axis=1)
-        attention_matrix = tf.matmul(tf.math.divide(similarity, self.key_size ** 0.5), Value)
+        similarity = tf.matmul(Query, tf.transpose(Key, perm=[0, 2, 1]))
+        similarity = tf.nn.softmax(tf.math.divide(similarity, tf.cast(self.key_size, dtype=inputs.dtype) ** 0.5), axis=2)
+        attention_matrix = tf.matmul(similarity, Value)
         return attention_matrix
 
 
@@ -52,47 +52,50 @@ class MultiHeadAttention(Layer):
         self.attention_layers = []
 
     def build(self, input_shape):
-        if input_shape[1] % self.n_channels != 0:
+        if input_shape[-1] % self.n_channels != 0:
             print("The embedding size needs to be divisible by the number of channels!")
             raise Exception
-        reduced_embedding = int(input_shape[1] / self.n_channels)
-
+        reduced_embedding = int(input_shape[-1] / self.n_channels)
         # Create stack of Attention layers
         for i in range(self.n_channels):
             self.attention_layers.append(DotProductAttention(reduced_embedding, reduced_embedding, reduced_embedding))
-
         # Initialize the last linear transform
         initializer = tf.initializers.GlorotNormal()
-        self.linear_transform = self.add_weight(shape=(input_shape[1], input_shape[1]), initializer=initializer,
+        self.linear_transform = self.add_weight(shape=(input_shape[-1], input_shape[-1]), initializer=initializer,
                                        trainable=True)
 
-    def call(self, inputs):
-        concatenated_result = 0
+    def call(self, inputs, *args, **kwargs):
+        concatenated_result = []
         for i in range(self.n_channels):
             # Each attention layer outputs an array with n_embedding in size, which concatenates to the full input shape
             attention_output = self.attention_layers[i](inputs)
 
-            # Concatenate the result
-            if i > 0:
-                concatenated_result = tf.concat([concatenated_result, attention_output], axis=1)
-            else:
-                concatenated_result = attention_output
-
+            # Store the results and then concatenate them
+            concatenated_result.append(attention_output)
+        # Concatenate the results
+        concatenated_result = tf.concat(concatenated_result, axis=2)
         # Perform the final linear transform
         output = tf.matmul(concatenated_result, self.linear_transform)
-
         return output
 
 
 class NormLayer(Layer):
-    def __init__(self):
+    def __init__(self, epsilon=10 ** (-8)):
         super(NormLayer, self).__init__()
+        self.scale = None
+        self.shift = None
+        self.epsilon = epsilon
 
-    def call(self, inputs, epsilon=10 ** (-8)):
+    def build(self, input_shape):
+        self.scale = self.add_weight(shape=(input_shape[-1],), initializer=tf.keras.initializers.Ones(), trainable=True)
+        self.shift = self.add_weight(shape=(input_shape[-1],), initializer=tf.keras.initializers.Zeros(), trainable=True)
+
+    def call(self, inputs, *args, **kwargs):
         # Normalize over hidden layers instead of training samples
-        mean = tf.reduce_mean(inputs, axis=1)
-        std = tf.math.reduce_std(inputs, axis=1)
-        outputs = (inputs - mean[:, tf.newaxis]) / (std[:, tf.newaxis] + epsilon)
+        mean = tf.reduce_mean(inputs, axis=2, keepdims=True)
+        std = tf.math.reduce_std(inputs, axis=2, keepdims=True)
+        x_norm = (inputs - mean) / (std + self.epsilon)
+        outputs = tf.multiply(x_norm, self.scale) + self.shift
         return outputs
 
 
@@ -117,8 +120,9 @@ class MLP(Layer):
         self.b1 = self.add_weight(shape=(self.hidden_units,), initializer=initializer, trainable=True)
         self.b2 = self.add_weight(shape=(input_shape[-1],), initializer=initializer, trainable=True)
 
-    def call(self, inputs, training):
+    def call(self, inputs, *args, **kwargs):
         # Normalize over hidden layers instead of training samples
+        training = kwargs.get('training', False)
         outputs = tf.matmul(inputs, self.W1) + self.b1
         outputs = keras.activations.gelu(outputs)
         outputs = keras.layers.Dropout(self.dropout, outputs.shape)(outputs, training)
@@ -127,13 +131,47 @@ class MLP(Layer):
         return outputs
 
 
-if __name__ == "__main__":
-    attention = MultiHeadAttention(4)
-    test = tf.constant([[10.0, 1.4, 1.3, 4.2], [-5.4, 3.2, 1.7, 3.2], [1.6, 5.1, 6.3, 1.2]])
+class EncoderBlock(Layer):
+    def __init__(self, n_channels, hidden_nodes, dropout=0.0):
+        super(EncoderBlock, self).__init__()
+        self.MLA = MultiHeadAttention(n_channels)
+        self.MLP = MLP(hidden_nodes, dropout)
+        self.LayerNorm1 = NormLayer()
+        self.LayerNorm2 = NormLayer()
 
-    norm = NormLayer()
-    mlp = MLP(5, 0.1)
-    print(mlp(test, False))
-    print(norm(test))
-    print(attention(test))
+    def call(self, inputs, *args, **kwargs):
+        initial_inputs = inputs
+        normalized_inputs = self.LayerNorm1(inputs)
+        attention_output = self.MLA(normalized_inputs)
+        combined_output = attention_output + initial_inputs
+        normalized_inputs = self.LayerNorm2(combined_output)
+        feed_forward_output = self.MLP(normalized_inputs)
+        encoder_output = feed_forward_output + combined_output
+        return encoder_output
+
+
+class CLSTokenLayer(Layer):
+    def __init__(self, d_model):
+        super(CLSTokenLayer, self).__init__()
+        self.cls_token = self.add_weight(shape=(1, 1, d_model), initializer='zeros', trainable=True)
+
+    def call(self, inputs, *args, **kwargs):
+        # Repeat the cls token across the batch and concatenate with inputs
+        batch_size = tf.shape(inputs)[0]
+        cls_tokens = tf.repeat(self.cls_token, repeats=batch_size, axis=0)
+        return tf.concat([cls_tokens, inputs], axis=1)
+
+
+class PositionalEmbedding(Layer):
+    def __init__(self,):
+        super(PositionalEmbedding, self).__init__()
+        self.pos_embedding = None
+
+    def build(self, input_shape):
+        initializer = tf.initializers.GlorotNormal()
+        self.pos_embedding = self.add_weight(shape=(1, input_shape[1], input_shape[-1]), initializer=initializer)
+
+    def call(self, inputs, *args, **kwargs):
+        return inputs + self.pos_embedding
+
 
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -105,10 +105,10 @@ def VIT_dataprepocessing_model_phase(
     x_test,
     y_test,
     image_size: int = 32,
-    patch_qty: int = 32,
+    patch_qty: int = 16,
     size_per_patch: int = 8,
-    dimension_dense_projection: int = 16,
-    dense_activation: str = "relu",
+    dimension_dense_projection: int = 240,
+    dense_activation: str = "gelu",
     data_inputs=None,
     data_outputs=10,
     image_height=16,
@@ -118,7 +118,6 @@ def VIT_dataprepocessing_model_phase(
     input_dimensionality = 1 + patch_qty
     inputs = Input(shape=(image_size, image_size, 3), batch_size=None)
 
-    patch_qty = 16
     patches = Reshape((patch_qty, size_per_patch, size_per_patch, 3))(inputs)
 
     flattened_patches = Reshape(
@@ -130,16 +129,9 @@ def VIT_dataprepocessing_model_phase(
     )(flattened_patches)
 
     batch_size = x_train.shape[1]
-    cls = np.zeros(
-        (batch_size, 1, dimension_dense_projection), dtype="float32"
-    )
-    cls = tf.convert_to_tensor(cls)  # Convert to TensorFlow tensor
 
-    projection = tf.convert_to_tensor(projection)  # Convert to TensorFlow tensor
+    sequential_vectors_non_processed = CLSTokenLayer(dimension_dense_projection)(projection)
 
-    sequential_vectors_non_processed = tf.concat(
-        [cls, projection], axis=1
-    )
     sequential_vectors = tf.reshape(
         sequential_vectors_non_processed,
         [-1, patch_qty + 1, dimension_dense_projection],
@@ -152,32 +144,19 @@ def VIT_dataprepocessing_model_phase(
         input_dim=input_dimensionality, output_dim=dimension_dense_projection
     )(positions)
 
-    encoded_layer = positional_embeddings + sequential_vectors
+    encoded_layer = PositionalEmbedding()(sequential_vectors)
+    encoder_output = encoded_layer
 
-    attentionlayer = MultiHeadAttention(4)
+    n_encoder_blocks = 12
+    for i in range(n_encoder_blocks):
+        encoder_output = EncoderBlock(12, 4 * dimension_dense_projection, dropout=0.1)(encoder_output)
 
-    normlayer = NormLayer()
-
-    layernormalized = normlayer(encoded_layer)
-
-    transformerprocessed = attentionlayer(layernormalized)
-
-    transformerprocessed = layers.Add()([transformerprocessed, encoded_layer])
-
-    layernormalized2 = NormLayer()(transformerprocessed)
-
-    mlpprocessed = MLP(20, dropout=0.0)(layernormalized2)
-
-    processed = layers.Add()([mlpprocessed, transformerprocessed])
-
-    outputs = layers.Softmax(axis=1)(layers.Dense(units=data_outputs,
-                           name='head')(processed)[:, 0, :])
-
+    outputs = layers.Softmax(axis=1)(layers.Dense(units=data_outputs, name='head')(encoder_output[:, 0]))
     model = Model(inputs, outputs)
 
     print(model.summary())
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    optimizer = tf.keras.optimizers.Adam()
     model.compile(optimizer=optimizer,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
@@ -185,7 +164,7 @@ def VIT_dataprepocessing_model_phase(
     train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     valid_data = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
-    stats = model.fit(train_data, epochs=50, batch_size=batch_size)
+    stats = model.fit(train_data, validation_data=valid_data, epochs=50, batch_size=batch_size)
 
     preds = model.predict(valid_data)
 
@@ -197,15 +176,14 @@ def VIT_dataprepocessing_model_phase(
     # return Model(
     #     inputs=inputs, outputs=positional_embeddings + sequential_vectors
     # )
+
 import keras.activations
 import tensorflow as tf
 from keras import layers, Model
-from keras.initializers import GlorotNormal
 from keras.layers import Reshape, Input, Dense, Layer
 import numpy as np
 import pickle
 from typing import Dict, Union, Any
-
 
 
 class DotProductAttention(Layer):
@@ -230,7 +208,7 @@ class DotProductAttention(Layer):
         self.W_value = self.add_weight(shape=(input_shape[-1], self.value_size), initializer=initializer,
                                        trainable=True)
 
-    def call(self, inputs):
+    def call(self, inputs, *args, **kwargs):
         # Did not implement a mask - Don't know if it is required
 
         # Compute the Q, K, V matrices as given in attention is all you need
@@ -238,8 +216,9 @@ class DotProductAttention(Layer):
         Key = tf.matmul(inputs, self.W_key)
         Value = tf.matmul(inputs, self.W_value)
         # Calculate similarity measure and apply softmax along columns
-        similarity = tf.nn.softmax(tf.matmul(Query, tf.transpose(Key, perm=[0, 2, 1])), axis=1)
-        attention_matrix = tf.matmul(tf.math.divide(similarity, self.key_size ** 0.5), Value)
+        similarity = tf.matmul(Query, tf.transpose(Key, perm=[0, 2, 1]))
+        similarity = tf.nn.softmax(tf.math.divide(similarity, tf.cast(self.key_size, dtype=inputs.dtype) ** 0.5), axis=2)
+        attention_matrix = tf.matmul(similarity, Value)
         return attention_matrix
 
 
@@ -255,42 +234,46 @@ class MultiHeadAttention(Layer):
             print("The embedding size needs to be divisible by the number of channels!")
             raise Exception
         reduced_embedding = int(input_shape[-1] / self.n_channels)
-
         # Create stack of Attention layers
         for i in range(self.n_channels):
             self.attention_layers.append(DotProductAttention(reduced_embedding, reduced_embedding, reduced_embedding))
-
         # Initialize the last linear transform
         initializer = tf.initializers.GlorotNormal()
         self.linear_transform = self.add_weight(shape=(input_shape[-1], input_shape[-1]), initializer=initializer,
                                        trainable=True)
 
-    def call(self, inputs):
-        concatenated_result = 0
+    def call(self, inputs, *args, **kwargs):
+        concatenated_result = []
         for i in range(self.n_channels):
             # Each attention layer outputs an array with n_embedding in size, which concatenates to the full input shape
             attention_output = self.attention_layers[i](inputs)
 
-            # Concatenate the result
-            if i > 0:
-                concatenated_result = tf.concat([concatenated_result, attention_output], axis=2)
-            else:
-                concatenated_result = attention_output
-
+            # Store the results and then concatenate them
+            concatenated_result.append(attention_output)
+        # Concatenate the results
+        concatenated_result = tf.concat(concatenated_result, axis=2)
         # Perform the final linear transform
         output = tf.matmul(concatenated_result, self.linear_transform)
-
         return output
 
-class NormLayer(Layer):
-    def __init__(self):
-        super(NormLayer, self).__init__()
 
-    def call(self, inputs, epsilon=10 ** (-8)):
+class NormLayer(Layer):
+    def __init__(self, epsilon=10 ** (-8)):
+        super(NormLayer, self).__init__()
+        self.scale = None
+        self.shift = None
+        self.epsilon = epsilon
+
+    def build(self, input_shape):
+        self.scale = self.add_weight(shape=(input_shape[-1],), initializer=tf.keras.initializers.Ones(), trainable=True)
+        self.shift = self.add_weight(shape=(input_shape[-1],), initializer=tf.keras.initializers.Zeros(), trainable=True)
+
+    def call(self, inputs, *args, **kwargs):
         # Normalize over hidden layers instead of training samples
-        mean = tf.reduce_mean(inputs, axis=1)
-        std = tf.math.reduce_std(inputs, axis=1)
-        outputs = (inputs - mean[:, tf.newaxis]) / (std[:, tf.newaxis] + epsilon)
+        mean = tf.reduce_mean(inputs, axis=2, keepdims=True)
+        std = tf.math.reduce_std(inputs, axis=2, keepdims=True)
+        x_norm = (inputs - mean) / (std + self.epsilon)
+        outputs = tf.multiply(x_norm, self.scale) + self.shift
         return outputs
 
 
@@ -315,8 +298,9 @@ class MLP(Layer):
         self.b1 = self.add_weight(shape=(self.hidden_units,), initializer=initializer, trainable=True)
         self.b2 = self.add_weight(shape=(input_shape[-1],), initializer=initializer, trainable=True)
 
-    def call(self, inputs, training):
+    def call(self, inputs, *args, **kwargs):
         # Normalize over hidden layers instead of training samples
+        training = kwargs.get('training', False)
         outputs = tf.matmul(inputs, self.W1) + self.b1
         outputs = keras.activations.gelu(outputs)
         outputs = keras.layers.Dropout(self.dropout, outputs.shape)(outputs, training)
@@ -324,3 +308,46 @@ class MLP(Layer):
         outputs = keras.layers.Dropout(self.dropout, outputs.shape)(outputs, training)
         return outputs
 
+
+class EncoderBlock(Layer):
+    def __init__(self, n_channels, hidden_nodes, dropout=0.0):
+        super(EncoderBlock, self).__init__()
+        self.MLA = MultiHeadAttention(n_channels)
+        self.MLP = MLP(hidden_nodes, dropout)
+        self.LayerNorm1 = NormLayer()
+        self.LayerNorm2 = NormLayer()
+
+    def call(self, inputs, *args, **kwargs):
+        initial_inputs = inputs
+        normalized_inputs = self.LayerNorm1(inputs)
+        attention_output = self.MLA(normalized_inputs)
+        combined_output = attention_output + initial_inputs
+        normalized_inputs = self.LayerNorm2(combined_output)
+        feed_forward_output = self.MLP(normalized_inputs)
+        encoder_output = feed_forward_output + combined_output
+        return encoder_output
+
+
+class CLSTokenLayer(Layer):
+    def __init__(self, d_model):
+        super(CLSTokenLayer, self).__init__()
+        self.cls_token = self.add_weight(shape=(1, 1, d_model), initializer='zeros', trainable=True)
+
+    def call(self, inputs, *args, **kwargs):
+        # Repeat the cls token across the batch and concatenate with inputs
+        batch_size = tf.shape(inputs)[0]
+        cls_tokens = tf.repeat(self.cls_token, repeats=batch_size, axis=0)
+        return tf.concat([cls_tokens, inputs], axis=1)
+
+
+class PositionalEmbedding(Layer):
+    def __init__(self,):
+        super(PositionalEmbedding, self).__init__()
+        self.pos_embedding = None
+
+    def build(self, input_shape):
+        initializer = tf.initializers.GlorotNormal()
+        self.pos_embedding = self.add_weight(shape=(1, input_shape[1], input_shape[-1]), initializer=initializer)
+
+    def call(self, inputs, *args, **kwargs):
+        return inputs + self.pos_embedding
